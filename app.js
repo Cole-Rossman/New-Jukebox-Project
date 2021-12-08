9afc67346c7c5ade9bda364b90bcec9296363b09
Cole Rossman <colerossman99@gmail.com>
2021-12-08 15:56:15 -0800

    Latest Changes 3:55

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 import { ozzysounds } from './data.js';
-
 console.log(ozzysounds);
-
-
+const template = document.getElementById('template');
+const list = document.querySelector('#list');
 const audio = document.getElementById('audio');
+console.log(list);
+for (let ozzy of ozzysounds) {
+  console.log(ozzy);
+  const copy = template.content.cloneNode(true);
+  const header = copy.querySelector('h2');
+  const image = copy.querySelector('img');
+  const button = copy.querySelector('button');
+  header.textContent = ozzy.name;
+  image.src = ozzy.image;
 
-const ozzySound1 = document.getElementById('ozzy-sound1');
-ozzySound1.addEventListener('click', () => {
-  audio.src = 'assets/sounds/ozzysound1.mp3';
-  audio.play();
-});
+  button.addEventListener('click', () => {
+    audio.src = ozzy.audio;
+    audio.play();
 
-const ozzySound2 = document.getElementById('ozzy-sound2');
-ozzySound2.addEventListener('click', () => {
-  audio.src = 'assets/sounds/ozzysound2.mp3';
-  audio.play();
-});
+  });
 
-const ozzySound3 = document.getElementById('ozzy-sound3');
-ozzySound3.addEventListener('click', () => {
-  audio.src = 'assets/sounds/ozzysound3.mp3';
-  audio.play();
-});
+list.appendChild(copy);
+
+}
