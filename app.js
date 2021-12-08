10675b5ca0b9ac2cab0eb71f0fd65144f16287fb
Cole Rossman <colerossman99@gmail.com>
2021-12-08 11:53:57 -0800

    Ozzy Updates

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const audio = document.getElementById('audio');
 
 const ozzySound1 = document.getElementById('ozzy-sound1');
 ozzySound1.addEventListener('click', () => {
-audio.src = 'assets/sounds/ozzysound1.mp3';
-audio.play();
+  audio.src = 'assets/sounds/ozzysound1.mp3';
+  audio.play();
 });
 
 const ozzySound2 = document.getElementById('ozzy-sound2');
 ozzySound2.addEventListener('click', () => {
-audio.src = 'assets/sounds/ozzysound2.mp3';
-audio.play();
+  audio.src = 'assets/sounds/ozzysound2.mp3';
+  audio.play();
 });
 
 const ozzySound3 = document.getElementById('ozzy-sound3');
 ozzySound3.addEventListener('click', () => {
-audio.src = 'assets/sounds/ozzysound3.mp3';
-audio.play();
+  audio.src = 'assets/sounds/ozzysound3.mp3';
+  audio.play();
 });
